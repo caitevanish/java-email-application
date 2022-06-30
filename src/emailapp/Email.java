84d1c546210417ae8be475ef2668e9368397248e
caitevanish <cait.evan@gmail.com>
2022-06-30 11:20:20 -0700

    Generate a Random Password. Edited some print lines for clarity

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private int mailboxCapacity;
+	private int defaultPasswordLength =10;
 	private String alternateEmail;
 
 	//Constructor to receive the first and last name
@@ -20,11 +21,14 @@ public class Email {
 		this.department = setDepartment();
 		System.out.println("Department: " + this.department);
 	
+		//Call a method that returns a random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("\nYour password is: " + this.password);
 	}
 	
 	//Ask for the department
 	private String setDepartment() {
-		System.out.println("DEPARTMENT CODES: \n1 for Sales \n2 for Development \n3 for Accounting \n0 for none\nEnter department code:" );
+		System.out.println("\nDEPARTMENT CODES: \n[1] for Sales \n[2] for Development \n[3] for Accounting \n[0] for none\n\nEnter department code:" );
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) {return "sales";}
@@ -34,6 +38,19 @@ public class Email {
 	}
 	
 	//generate a random password
+	private String randomPassword(int length) {
+		
+		//generate pass within scope of string
+		String passwordSetString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+		char[] password = new char[length];
+		
+		//take the char at the random number and pass into password array one at a time
+		for (int i=0; i<length; i++) {
+			int rand = (int) (Math.random() *passwordSetString.length());
+			password[i] = passwordSetString.charAt(rand);
+		}
+		return new String(password);
+	}
 	
 	//set the mailbox capacity
 	
